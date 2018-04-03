d20fe92db03e685b11ddafb23237221a4bf62cee
Ravindra <rpkharche@gmail.com>
2018-04-03 20:13:56 +0530

    Correcting README and setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pybseindia
+# python-bseindia
 API for fetching publicly available information from Bombay Stock Exchange website - BSE.
 # What is it?
 This project provides the api for integrating data available on Bombay Stock Exchange website into any project. 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='pybseindia',
       version='0.1',
       description='API to access data on Bombay Stock Exchange',
-      url='https://github.com/rpkharche/pybseindia',
+      url='https://github.com/rpkharche/python-bseindia',
       author='Ravindra Kharche',
       author_email='rpkharche@gmail.com',
       license='MIT',

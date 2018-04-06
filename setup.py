bd99906b1b7874041f74d53672bbde3b0724835f
Ravindra <rpkharche@gmail.com>
2018-04-06 10:55:10 +0530

    Corrected package name.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-"""Setup script for pyBSE
+"""Setup script for python-bseindia
 """
 from setuptools import setup
 
-setup(name='pybseindia',
+setup(name='python-bseindia',
       version='0.1',
       description='API to access data on Bombay Stock Exchange',
       url='https://github.com/rpkharche/python-bseindia',
       author='Ravindra Kharche',
       author_email='rpkharche@gmail.com',
       license='MIT',
-      packages=['pybseindia'],
+      packages=['python-bseindia'],
 	  install_requires=[
           'pytest',
 		  'beautifulsoup4'
